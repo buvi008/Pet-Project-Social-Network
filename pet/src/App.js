b2edc02279a2819646bcb77d3bae1ec4b6da2112
Djambulat <buvi008@mail.ru>
2022-02-07 20:05:40 +0300

    Start

---
+++ pet/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <img src="https://media.istockphoto.com/photos/gold-letter-s-picture-id621931416" alt="" />
+    </div>
+  );
+}
+
+export default App;
